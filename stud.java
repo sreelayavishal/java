9f8e4fa99f35bed58ea1d167a2a22df152762fd0
Sreelaya Vishal <93375670+sreelayavishal@users.noreply.github.com>
2022-03-21 11:10:40 +0530

    stud name,roll

---
+++ stud.java
@@ -0,0 +1,21 @@
+package sreelaya.java;
+import java.util.Scanner;
+public class stud {
+	
+	public static void main(String args[])
+	{
+	       Scanner sc = new Scanner(System.in);
+	       System.out.println("Enter name of student: ");
+	       String n = sc.nextLine();
+	       System.out.println("Enter roll no of student: ");
+	       int c = sc.nextInt();
+	       System.out.println("Enter mark of student: ");
+	       int r = sc.nextInt();
+	         System.out.println("roll no:"+c);
+	         System.out.println("name:"+n);
+	          System.out.println("mark:"+r);
+
+	}
+	}
+
+
